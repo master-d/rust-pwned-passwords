c565a49d1dcc955f1cf6db3f3d543fa675f6047b
master-d <goodeveningthisisrobrichards@gmail.com>
2017-09-11 22:12:06 -0500

    added -f option to create hash dir structure

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,27 @@ fn check_pwd(password: &[u8]) {
     println!("{} {} bytes",hash, hash.len());
 }
 
-fn get_file_from_hash(hash: &String) -> fs::File {
+fn get_file_from_hash(hash: &String, create_file: bool) -> Option<fs::File> {
         let folder_bytes = &hash[..2];
         let file_bytes = &hash[2..4];
-        let filename = format!("hashes/{}/{}.txt",folder_bytes,file_bytes);
+        let dir = format!("hashes/{}",folder_bytes);
+        let filename = format!("{}/{}.txt",dir,file_bytes);
+        if create_file {
+            match fs::create_dir_all(&dir) {
+                Ok(d) => d,
+                Err(e) => panic!("Could not create folder {}",dir)
+            };
+        }
         let file = match fs::OpenOptions::new()
             .read(true)
             .write(true)
-            .create(true)
+            .create(create_file)
             .open(filename) {
-                Ok(file) => file,
-                Err(e) => panic!("{}", e)
+                Ok(file) => Some(file),
+                Err(e) => {
+                    println!("file error: {}", e);
+                    None
+                }
             };
         return file;
 }
@@ -38,20 +48,33 @@ fn update_hash_fs(file_path: &String) {
     let mut br = BufReader::new(ufile);
     for line_r in br.lines() {
         let line = match line_r {
-            Ok(line) => line,
+            Ok(line) => {
+                match get_file_from_hash(&line, true) {
+                    Some(mut file) => { 
+                        println!("Opened File for hash:{}", line);
+                        seek_bs(&file);
+                        file.write(format!("{}\n",line).as_bytes());
+                    },
+                    None => panic!("Could not create/open file for hash:{}", line)
+                }
+            },
             Err(e) => panic!("{}", "Error reading line of file")
         };
-        let mut hash_file = get_file_from_hash(&line);
-        hash_file.write(line.as_bytes());
         //println!("{} {} {}", hash_folder, hash_file, line);
     }
+}
+
+fn seek_bs(file: &fs::File) {
+    
 }
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        1 => println!("Please enter a password to check for pwnage"),
+        1 => println!("Usage: rpwned [PASSWORD]\n\
+        Check password for pwnage.\n\n\
+        \t-f [HASH_FILE]\tcreate hash file structure from pwned password file or update file"),
         2 => check_pwd(args[1].as_bytes()),
         _ => {
             // check 1st argument to see what operation user wants to perform
